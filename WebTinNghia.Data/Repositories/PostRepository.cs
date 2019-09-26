d63fe443829f4fde01513862c39a640520fb0c6c
HongPhong1872 <hongphong1872@gmail.com>
2019-09-26 11:05:32 +0700

    update service

--- WebTinNghia.Data/Repositories/PostRepository.cs
+++ WebTinNghia.Data/Repositories/PostRepository.cs
@@ -1,16 +1,36 @@
-﻿using WebTinNghia.Data.Infrastructure;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using WebTinNghia.Data.Infrastructure;
 using WebTinNghia.Model.Models;
+using System.Linq;
 
 namespace WebTinNghia.Data.Repositories
 {
     public interface IPostRepository : IRepository<Post>
     {
+        IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow);
+        IEnumerable<Post> GetMultiPaging(string tag, int page, int pageSize, out int totalRow);
+        IEnumerable<Post> GetMultiPaging(Func<object, object> p);
     }
 
     public class PostRepository : RepositoryBase<Post>, IPostRepository
     {
         public PostRepository(IDbFactory dbFactory) : base(dbFactory)
         {
+        }
+
+        public IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow)
+        {
+            var query = from p in DbContext.Posts
+                        join pt in DbContext.PostTags
+                        on p.ID equals pt.PostID
+                        where pt.TagID == tag && p.Status
+                        orderby p.CreatedDate descending
+                        select p;
+            totalRow = query.Count();
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            return query;
         }
     }
 }
